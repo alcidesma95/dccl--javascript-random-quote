562366efbf217f107a002f43d91193527cf40397
AlcidesMa <alcidesma95@gmail.com>
2023-10-29 15:24:26 -0400

    challenge finished

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
-const quote ={
-  _id:"a_MqBnMhXblP",
-  content:"Only those who dare to fail greatly can ever achieve greatly.",
-  author:"Robert F. Kennedy",
-  tags:["Famous Quotes"],
-  authorSlug:"robert-f-kennedy",
-  length:61,
-  dateAdded:"2019-03-15",
-  dateModified:"2023-04-14"
+const url = 'https://api.quotable.io/random'
+const quoteEl = document.getElementById('quote')
+const tagsEl = document.getElementById('tags')
+const authorEl = document.getElementById('author')
+const roll = document.getElementById('roll')
+const share = document.getElementById('share')
+
+const rs= {
+"_id":"LpBt1VcHO",
+"content":"Love is flower like; Friendship is like a sheltering tree.",
+"author":"Samuel Taylor Coleridge",
+"tags":["Friendship"],
+"authorSlug":"samuel-taylor-coleridge",
+"length":58,
+"dateAdded":"2020-02-27",
+"dateModified":"2023-04-14"
 }
+
+function getData(){
+ return fetch(url)
+  .then(res=> res.json())
+  .then(res =>  res)
+  .catch(err => console.log(err))
+}
+
+async function setData(){
+  const {content, author, tags} = await getData()
+  while (tagsEl.firstChild) {
+    tagsEl.removeChild(tagsEl.firstChild);
+  }
+  quoteEl.textContent = content
+  authorEl.textContent = author
+  tags.map(
+    tag=>{
+    const newTag = document.createElement('li')
+    newTag.textContent = tag
+    tagsEl.append(newTag)
+
+    }
+  )
+}
+
+setData()
+
+roll.addEventListener('click', setData)
+
+
+function clip(text){
+  navigator.clipboard.writeText(quoteEl.textContent)
+  .then(()=>
+      alert('✅ copy to clipboard')
+    )
+    .catch((err)=>
+      alert('❌ error to copy')
+    )
+}
+
+share.addEventListener('click', clip)
