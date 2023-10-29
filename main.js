d4bcb29a1b838f58f4fb6526e5f1c6618a267389
AlcidesMa <alcidesma95@gmail.com>
2023-10-28 23:15:27 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quote ={
+  _id:"a_MqBnMhXblP",
+  content:"Only those who dare to fail greatly can ever achieve greatly.",
+  author:"Robert F. Kennedy",
+  tags:["Famous Quotes"],
+  authorSlug:"robert-f-kennedy",
+  length:61,
+  dateAdded:"2019-03-15",
+  dateModified:"2023-04-14"
+}
